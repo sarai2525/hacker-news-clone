2dae5805ccb2a7e7773ed00548ec6f45fafa3dd8
sarai2525 <s.arai@newell-productions.com>
2023-05-28 03:20:10 +0900

    fix: rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   rules: {},
   overrides: [
     {
-      files: ['src/pages/*.vue'],
+      files: ['src/pages/**/*.vue'],
       rules: {
         'vue/multi-word-component-names': 'off',
       },
