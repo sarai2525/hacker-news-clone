0100d0e7c715271752ff26ba3d69560de12d281d
sarai2525 <s.arai@newell-productions.com>
2023-05-28 03:15:24 +0900

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,19 +5,19 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  parser: "vue-eslint-parser",
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
   },
   extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', '@vue/prettier'],
   plugins: [],
   rules: {},
   overrides: [
     {
-      files: ["src/pages/*.vue"],
+      files: ['src/pages/*.vue'],
       rules: {
-        "vue/multi-word-component-names": "off",
+        'vue/multi-word-component-names': 'off',
       },
     },
   ],
-};
+}
