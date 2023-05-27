eaa3ab0ee2f4a365cc5d20c82a9d6d94d3ecf306
sarai2525 <s.arai@newell-productions.com>
2023-05-27 23:12:10 +0900

    create: bff server

---
+++ server/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+const router = express.Router();
+
+app.use(express.json());
+
+app.use((_req, _res, next) => {
+  console.log("log");
+  next();
+});
+
+router.get("/error", (_req, res, next) => {
+  next(new Error("hoge"));
+});
+
+router.get("/hello", (_req, res, _next) => {
+  res.json({ message: "hello world" });
+});
+
+app.use("/api", router);
+
+export default fromNodeMiddleware(app);
