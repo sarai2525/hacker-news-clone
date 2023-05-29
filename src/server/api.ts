5893dd9fc3fd6177ba7fbbe7b6ac3e0297dc7675
smileX2_merries <s.arai@newell-productions.com>
2023-05-30 07:52:24 +0900

    Router setup (#9)

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,24 +1,9 @@
-import express, { Router } from 'express'
-import { tokyoWeatherForecastService } from './service/fetchTokyoWeatherForecast'
+import express from 'express'
+import router from './router/index'
 
 const app = express()
-const router = Router()
 app.use(express.json())
-
-// Move to server/express-app/routes/test.ts, anywhere really, but avoid server/api and server/middleware
-router.get('/tokyo-weather-forecast', async (req, res, next) => {
-  try {
-    const result = await tokyoWeatherForecastService()
-    res.status(200).json(result)
-  } catch (err) {
-    next(err)
-  }
-})
-
-// FIXME: cannot add router path
-// Error: Router.use() requires a middleware function but got a string
 app.use('/api/', router)
-
 // Catch-all
 app.use((_req, res) => res.status(200).json({ route: '/*' }))
 
