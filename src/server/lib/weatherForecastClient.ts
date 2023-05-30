84d51bd1e7ac683fec018f90a1299dd3d1d3520f
smileX2_merries <s.arai@newell-productions.com>
2023-05-30 09:10:11 +0900

    add log from client (#11)

--- src/server/lib/weatherForecastClient.ts
+++ src/server/lib/weatherForecastClient.ts
@@ -1,7 +1,19 @@
 // API https://open-meteo.com/en/docs#latitude=35.69&longitude=139.69&hourly=temperature_2m
 
 import { ofetch, $Fetch } from 'ofetch'
+import { consola } from 'consola'
+
 const BASE_URL = 'https://api.open-meteo.com/v1'
+
+type RequestQuery = {
+  latitude: number
+  longitude: number
+  hourly?: string
+  daily?: string
+  timezone: string
+}
+
+//api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum&timezone=Asia%2FTokyo
 
 class WeatherForecastClient {
   private client: $Fetch
@@ -11,17 +23,33 @@ class WeatherForecastClient {
       baseURL: BASE_URL,
       async onRequest({ request, options }) {
         const { baseURL } = options
-        console.log('[fetch request]', `${baseURL}${request}`)
+        consola.info({
+          message: `[Fetch request] ${baseURL}${request}`,
+          badge: true,
+        })
         options.query = options.query || {}
         options.query.t = new Date()
+      },
+      async onRequestError({ error }) {
+        consola.error({
+          message: error.message,
+          badge: true,
+        })
+      },
+      async onResponse({ response }) {
+        if (response.statusText === 'OK') {
+          consola.success('[Succeed to request]', response._data)
+          return
+        }
+        consola.error('[Failed to request]', response._data.reason)
       },
     })
   }
   public async getTokyoWeatherForecast() {
-    const query = {
+    const query: RequestQuery = {
       latitude: 35.6785,
       longitude: 139.6823,
-      hourly: 'temperature_2m',
+      daily: 'weathercode,temperature_2m_max,temperature_2m_min,rain_sum',
       timezone: 'Asia/Tokyo',
     }
     const result = await this.client('/forecast', {
