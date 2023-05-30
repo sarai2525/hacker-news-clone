8da4ab7e4ca68265492bec2e3932baf303fa67f9
Yahnis <yahnis.allegret@yahoo.com>
2023-05-30 19:22:24 +0200

    new client with ky

--- src/server/lib/weatherForecastClient.ts
+++ src/server/lib/weatherForecastClient.ts
@@ -1,10 +1,8 @@
-// API https://open-meteo.com/en/docs#latitude=35.69&longitude=139.69&hourly=temperature_2m
-
-import { ofetch, $Fetch } from 'ofetch'
-import { consola } from 'consola'
+import ky from 'ky'
 
 const BASE_URL = 'https://api.open-meteo.com/v1'
 
+// Type de requête
 type RequestQuery = {
   latitude: number
   longitude: number
@@ -13,38 +11,34 @@ type RequestQuery = {
   timezone: string
 }
 
-//api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum&timezone=Asia%2FTokyo
-
 class WeatherForecastClient {
-  private client: $Fetch
+  private client: typeof ky
 
   constructor() {
-    this.client = ofetch.create({
-      baseURL: BASE_URL,
-      async onRequest({ request, options }) {
-        const { baseURL } = options
-        consola.info({
-          message: `[Fetch request] ${baseURL}${request}`,
-          badge: true,
-        })
-        options.query = options.query || {}
-        options.query.t = new Date()
-      },
-      async onRequestError({ error }) {
-        consola.error({
-          message: error.message,
-          badge: true,
-        })
-      },
-      async onResponse({ response }) {
-        if (response.statusText === 'OK') {
-          consola.success('[Succeed to request]', response._data)
-          return
-        }
-        consola.error('[Failed to request]', response._data.reason)
+    this.client = ky.create({
+      prefixUrl: BASE_URL,
+      hooks: {
+        beforeRequest: [
+          (request) => {
+            const url = new URL(request.url)
+            url.searchParams.append('t', new Date().toISOString())
+
+            return new Request(url.toString(), request)
+          },
+        ],
+        afterResponse: [
+          (request, options, response) => {
+            if (response.ok) {
+              console.log('[Succeed to request]', response)
+              return response
+            }
+            console.error('[Failed to request]', response)
+          },
+        ],
       },
     })
   }
+
   public async getTokyoWeatherForecast() {
     const query: RequestQuery = {
       latitude: 35.6785,
@@ -52,11 +46,10 @@ class WeatherForecastClient {
       daily: 'weathercode,temperature_2m_max,temperature_2m_min,rain_sum',
       timezone: 'Asia/Tokyo',
     }
-    const result = await this.client('/forecast', {
-      query,
-    })
+    const result = await this.client.get('forecast', { searchParams: query }).json()
     return result
   }
+
   public async getFranceWeatherForecast() {
     const query: RequestQuery = {
       latitude: 46.0,
@@ -64,9 +57,7 @@ class WeatherForecastClient {
       daily: 'weathercode,temperature_2m_max,temperature_2m_min,rain_sum',
       timezone: 'Europe/Paris',
     }
-    const result = await this.client('/forecast', {
-      query,
-    })
+    const result = await this.client.get('forecast', { searchParams: query }).json()
     return result
   }
 }
