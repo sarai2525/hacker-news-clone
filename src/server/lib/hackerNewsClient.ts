28d442d41e1e9dac8be3fcd2059a48d4459c8046
Yahnis <yahnis.allegret@yahoo.com>
2023-06-06 18:25:20 +0200

    Start hacker news API

--- src/server/lib/hackerNewsClient.ts
+++ src/server/lib/hackerNewsClient.ts
@@ -12,16 +12,12 @@ class HackerNewsClient {
   }
 
   public async getBestHackerNews() {
-    // Obtenez d'abord la liste des ID des meilleures histoires
     const ids = await this.client.get('topstories.json').json<number[]>()
 
-    // Prenez les 10 premiers ID
     const top10Ids = ids.slice(0, 10)
 
-    // Créez une promesse pour chaque détail d'histoire que vous allez récupérer
     const storyPromises = top10Ids.map((id) => this.client.get(`item/${id}.json`).json())
 
-    // Exécutez toutes les promesses en parallèle et attendez qu'elles soient toutes résolues
     const top10Stories = await Promise.all(storyPromises)
 
     return top10Stories
